92fe3ae98fbb11e6440e27a209267b3a20136a44
brady.wang <brady.wang@qq.com>
2021-08-04 10:19:47 +0800

    cluster

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/usr/local/bin/redis-server /usr/local/cluster/7000/redis.conf
+/usr/local/bin/redis-server /usr/local/cluster/7001/redis.conf
+/usr/local/bin/redis-server /usr/local/cluster/7002/redis.conf
+/usr/local/bin/redis-server /usr/local/cluster/7003/redis.conf
+/usr/local/bin/redis-server /usr/local/cluster/7004/redis.conf
+/usr/local/bin/redis-server /usr/local/cluster/7005/redis.conf
